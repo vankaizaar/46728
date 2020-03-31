1368e9a73591df5955c0c227126a16bb66e86c5b
Zakaria Davis <zakariadavis@gmail.com>
2020-03-31 08:06:18 +0300

    Add template views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('46728.welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
